4b1f5b443d4200f9913f3ae1522469fa6718e6c2
mras2303 <marek.kowalonek@gmail.com>
2019-10-12 21:05:11 +0200

    Add frontend and fix babel build

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import config from './config';
 import { role, test, user } from './server/routes';
 import 'dotenv/config';
@@ -7,18 +8,21 @@ const app = express();
 
 
 config.useMiddleware(app);
-config.mongo_connect();
+config.useMongo();
 config.initNodeMailer();
 config.kickstartScheduler();
 
-app.get('/', (req, res) => {
-    res.send(`WORKS`)
-});
-app.use('/test', test);
-app.use('/role', role);
-app.use('/user', user);
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-const port = process.env.PORT || 1200;
+app.use('/api/test', test);
+app.use('/api/role', role);
+app.use('/api/user', user);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
 const server = app.listen(port, err => {
     if(err) throw err;
     console.log(`> Ready on port ${port}`);
