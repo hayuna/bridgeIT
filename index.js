da58118ecb593db2a2b8d17f62a1bbf7530c509d
mras2303 <marek.kowalonek@gmail.com>
2019-10-12 21:45:21 +0200

    fix prod workflow

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+console.log(process.env)
 const port = process.env.PORT || 5000;
 const server = app.listen(port, err => {
     if(err) throw err;
